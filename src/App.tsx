43792dba650d3a0445d22ecde4c4e6463012f09d
pluem <tw_pluem@hotmail.co.th>
2021-07-13 13:22:07 +0700

    [create] component

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,36 @@
-import React, { useState } from 'react'
-import logo from './logo.svg'
-import './App.css'
+import React, { useState } from "react";
+import logo from "./logo.svg";
+import "./App.css";
+import { Avatar } from "./components";
 
 function App() {
-  const [count, setCount] = useState(0)
+  const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <div className="p-5 bg-white grid justify-end">
+        <Avatar
+          href="#"
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          name={{ text: "Welcome!", textColor: "text-blue font-bold" }}
+          text={{ text: "@pokemon", textColor: "text-blue font-bold" }}
+        />
+      </div>
       <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="font-press-start">Hello Vite + React!</p>
+        <p className="font-press-start text-blue-dark">Hello Vite + React!</p>
         <p>
-          <button className="border-4 border-red-dark font-quicksand" type="button" onClick={() => setCount((count) => count + 1)}>
+          <button
+            className="border-4 border-red-dark font-quicksand text-blue-dark"
+            type="button"
+            onClick={() => setCount((count) => count + 1)}
+          >
             count is: {count}
           </button>
         </p>
-        <p className="font-quicksand">
+        <p className="font-quicksand text-blue-dark">
           Edit <code>App.tsx</code> and save to test HMR updates.
         </p>
-        <p>
+        <p className="font-press-start">
           <a
             className="App-link"
             href="https://reactjs.org"
@@ -27,7 +39,7 @@ function App() {
           >
             Learn React
           </a>
-          {' | '}
+          {" | "}
           <a
             className="App-link"
             href="https://vitejs.dev/guide/features.html"
@@ -39,7 +51,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
