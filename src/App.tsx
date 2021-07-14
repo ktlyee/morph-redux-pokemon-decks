dc849fffe3c56c9eafd77100defe79b342bafc8f
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-07-14 09:54:54 +0700

    [update] App.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,21 @@
 import React, { useState } from 'react'
 import logo from './logo.svg'
 import './App.css'
+import { Avatar } from "./components";
 
 function App() {
   const [count, setCount] = useState(0);
 
   return (
     <div className="App">
+      <div className="p-5 bg-white grid justify-end">
+        <Avatar
+          href="#"
+          src="https://images.unsplash.com/photo-1472099645785-5658abf4ff4e?ixlib=rb-1.2.1&ixid=eyJhcHBfaWQiOjEyMDd9&auto=format&fit=facearea&facepad=2&w=256&h=256&q=80"
+          name={{ text: "Welcome!", textColor: "text-blue font-bold" }}
+          text={{ text: "@pokemon", textColor: "text-blue font-bold" }}
+        />
+      </div>
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
         <p className="font-press-start">Hello Vite + React!</p>
@@ -39,7 +48,7 @@ function App() {
         </p>
       </header>
     </div>
-  )
+  );
 }
 
-export default App
+export default App;
