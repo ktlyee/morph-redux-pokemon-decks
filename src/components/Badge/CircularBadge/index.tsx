d1da5b82dcbd1f4d4cd01f660362729a87155835
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-07-17 21:20:50 +0700

    [update]: style badges

--- src/components/Badge/CircularBadge/index.tsx
+++ src/components/Badge/CircularBadge/index.tsx
@@ -5,18 +5,20 @@ interface CircularBadgeProps {
     src: string
     backgroundColor: string
     tooltip: string
-    alt?: string
+    alt?: string,
+    positionStyle?: string
 }
 
 const CircularBadge = ({
     src,
     alt,
     backgroundColor,
-    tooltip
+    tooltip,
+    positionStyle
 }: CircularBadgeProps) => {
     return (
         <Tooltip title={tooltip} arrow>
-            <div className={`inline-flex items-center p-2.5 border border-transparent rounded-full shadow-md ${backgroundColor}`}>
+            <div className={`inline-flex items-center p-2 border border-transparent rounded-full shadow-md ${positionStyle} ${backgroundColor}`}>
                 <img className='h-4 w-4' src={src} alt={alt} />
             </div>
         </Tooltip>
