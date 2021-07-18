fa15c14696063342218a475d5c4a22be65c140ae
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-07-18 20:55:42 +0700

    [add]: text component info card

--- src/InfoCardTest.tsx
+++ src/InfoCardTest.tsx
@@ -1,70 +1,83 @@
-import React from 'react'
-import { InfoCard } from './components/Card/InfoCard'
+import React, { useState } from 'react'
+import { InfoCard, Button } from './components'
 
 function InfoCardTest() {
+    const [isOpen, setIsOpen] = useState(false)
+
     return (
-        <InfoCard 
-            cardColor='bg-yellow'
-            pokemonImg={{ 
-                src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg',
-                alt: 'pikachu'
-            }}
-            backgroudTextColor='bg-yellow-lighter'
-            id='#001'
-            name='Pikachu'
-            tag={{
-                text: 'Mouse Pokemon',
-                backgroundColor: 'bg-blue'
-            }}
-            types={[
-                {
-                    src: 'https://pokedex-react-mui.netlify.app/electric.png',
-                    backgroundColor: 'bg-blue',
-                    tooltip: 'Elective'
-                },
-                {
-                    src: 'https://pokedex-react-mui.netlify.app/fire.png',
-                    backgroundColor: 'bg-blue',
-                    tooltip: 'Fire'
-                }
-            ]}
-            about='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla iaculis et mauris et vulputate. Vivamus id elit lacinia. Vivamus id elit lacinia.'
-            abilities={[
-                { ability: 'Lorem ipsum' },
-                { ability: 'Lorem ipsum' },
-                { ability: 'Lorem ipsum' }
-            ]}
-            baseStats={{
-                attack: '50',
-                defense: '45',
-                hp: '60',
-                specialAttack: '55',
-                specialDefense: '70',
-                speed: '40'
-            }}
-            evolution={[
-                {
-                    src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/172.svg',
-                    alt: 'evolution 1',
-                    href: '#',
-                    name: 'Pichu'
-                },
-                {
-                    src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg',
-                    alt: 'evolution 2',
-                    href: '#',
-                    name: 'Pikachu'
-                },
-                {
-                    src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/26.svg',
-                    alt: 'evolution 3',
-                    href: '#',
-                    name: 'Raichu'
-                }
-            ]}
-            circleColor='bg-yellow-lightest'
-            handleClose={() => {}}
-        />
+        <>
+            <Button
+                text={{ text: 'Open card', color: 'text-white'}}
+                buttonColor='bg-red'
+                handleClick={() => setIsOpen(true)}
+            />
+            {
+                isOpen &&
+                <InfoCard 
+                    cardColor='bg-yellow'
+                    pokemonImg={{ 
+                        src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg',
+                        alt: 'pikachu'
+                    }}
+                    backgroudTextColor='bg-yellow-lighter'
+                    id='#001'
+                    name='Pikachu'
+                    tag={{
+                        text: 'Mouse Pokemon',
+                        backgroundColor: 'bg-blue'
+                    }}
+                    types={[
+                        {
+                            src: 'https://pokedex-react-mui.netlify.app/electric.png',
+                            backgroundColor: 'bg-blue',
+                            tooltip: 'Elective'
+                        },
+                        {
+                            src: 'https://pokedex-react-mui.netlify.app/fire.png',
+                            backgroundColor: 'bg-blue',
+                            tooltip: 'Fire'
+                        }
+                    ]}
+                    about='Lorem ipsum dolor sit amet, consectetur adipiscing elit. Nulla iaculis et mauris et vulputate. Vivamus id elit lacinia. Vivamus id elit lacinia.'
+                    abilities={[
+                        { ability: 'Lorem ipsum' },
+                        { ability: 'Lorem ipsum' },
+                        { ability: 'Lorem ipsum' }
+                    ]}
+                    baseStats={{
+                        attack: '50',
+                        defense: '45',
+                        hp: '60',
+                        specialAttack: '55',
+                        specialDefense: '70',
+                        speed: '40'
+                    }}
+                    evolution={[
+                        {
+                            src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/172.svg',
+                            alt: 'evolution 1',
+                            href: '#',
+                            name: 'Pichu'
+                        },
+                        {
+                            src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/25.svg',
+                            alt: 'evolution 2',
+                            href: '#',
+                            name: 'Pikachu'
+                        },
+                        {
+                            src: 'https://raw.githubusercontent.com/PokeAPI/sprites/master/sprites/pokemon/other/dream-world/26.svg',
+                            alt: 'evolution 3',
+                            href: '#',
+                            name: 'Raichu'
+                        }
+                    ]}
+                    circleColor='bg-yellow-lightest'
+                    isOpen={isOpen}
+                    handleClose={() => setIsOpen(false)}
+                />
+            } 
+        </>
     )
 }
 
--- src/components/Card/InfoCard/index.tsx
+++ src/components/Card/InfoCard/index.tsx
@@ -1,6 +1,8 @@
-import React from 'react'
+import React, { Fragment, useRef } from 'react'
+import { Dialog, Transition } from '@headlessui/react'
 import { BasicBadge, CircularBadge } from '../../../components'
 import { ArrowNarrowRightIcon, XIcon } from '@heroicons/react/outline'
+import './infoCard.css'
 
 interface IPokemonImg {
     src: string
@@ -51,6 +53,7 @@ interface InfoCardProps {
     baseStats: IBaseStats
     evolution: IEvolution[],
     circleColor: string
+    isOpen: boolean
     handleClose: () => void
 }
 
@@ -67,147 +70,152 @@ export const InfoCard = ({
     baseStats,
     evolution,
     circleColor,
+    isOpen,
     handleClose
 }: InfoCardProps) => {
-    return (
-        <div className='flex items-center px-6 bg-white-smoke justify-center min-h-screen text-center sm:block sm:p-0'>
-            {/* This element is to trick the browser into centering the modal contents. */}
-            <span className='hidden sm:inline-block sm:align-middle sm:h-screen' aria-hidden='true'>
-                &#8203;
-            </span>
+    const completeButtonRef = useRef(null)
 
-            <div className={`m-4 py-5 px-4 inline-block ${cardColor} rounded-3xl text-left shadow-xl transform transition-all sm:align-middle sm:p-6 sm:w-10/12 md:w-10/12 lg:w-7/12 xl:w-7/12`}>
-                <div className="sm:block absolute top-0 right-0 pt-4 pr-4">
-                    <button
-                        type="button"
-                        className="bg-white rounded-full p-2 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-transparent"
-                        onClick={handleClose}
-                    >
-                        <XIcon className="h-6 w-6" aria-hidden="true" />
-                    </button>
-                </div>
-                <div className='grid grid-flow-row auto-rows-max'>
-                    {/* first row */}
-                    <div className='grid grid-cols-1 xl:grid-cols-2'>
-                        <div className='inline-flex items-center'>
-                            <img 
-                                src={`${pokemonImg.src}`} 
-                                alt={`${pokemonImg.alt}`}
-                                className='h-36 w-36 rounded-2xl mx-4'
-                            />
-                            <div>
-                                <p className='text-2xl font-normal font-quicksand'>{id}</p>
-                                <p className='text-lg font-press-start pt-2'>{name}</p>
-                                <div className='space-x-1 flex flex-wrap items-center mt-5'>
-                                    <BasicBadge 
-                                        text={`${tag.text}`} 
-                                        textStyle='text-xl leading-8 font-medium text-white font-vt323' 
-                                        backgroundColor={`${tag.backgroundColor}`}
-                                    />
-                                    {
-                                        types.map((type) => (
-                                            <CircularBadge 
-                                                src={`${type.src}`} 
-                                                backgroundColor={`${type.backgroundColor}`}
-                                                tooltip={`${type.tooltip}`}
-                                            />
-                                        ))
-                                    }
-                                </div>
+    return (
+        <Transition.Root show={isOpen} as={Fragment}>
+            <Dialog
+                as="div" 
+                static 
+                className='fixed z-10 inset-0 overflow-y-auto' 
+                initialFocus={completeButtonRef}
+                open={isOpen} 
+                onClose={handleClose}
+            >
+                <div className='flex items-center px-6 justify-center min-h-screen text-center sm:block sm:p-0'>
+                    <Transition.Child as={Fragment}>
+                        <Dialog.Overlay className='fixed inset-0 bg-white-smoke transition-opacity' />
+                    </Transition.Child>
+
+                    {/* This element is to trick the browser into centering the modal contents. */}
+                    <span className='hidden sm:inline-block sm:align-middle sm:h-screen' aria-hidden='true'>
+                        &#8203;
+                    </span>
+
+                    <Transition.Child as={Fragment}>
+                        <div className={`m-4 py-5 px-4 inline-block ${cardColor} rounded-3xl text-left shadow-xl transform transition-all sm:align-middle sm:p-6 sm:w-10/12 md:w-10/12 lg:w-7/12 xl:w-7/12`}>
+                            <div className="sm:block absolute top-0 right-0 pt-4 pr-4">
+                                <button
+                                    type="button"
+                                    className="bg-white rounded-full p-2 text-gray-400 hover:text-gray-500 focus:outline-none focus:ring-transparent"
+                                    onClick={handleClose}
+                                >
+                                    <XIcon className="h-6 w-6" aria-hidden="true" />
+                                </button>
                             </div>
-                        </div>
-                        <div className='border-l-2 border-gray-600 flex flex-wrap content-center pr-20 pl-10 mt-4 xl:ml-8'>
-                            <p className='text-sm font-press-start'>About</p>
-                            <p className='font-quicksand font-semibold text-sm text-gray-700 pt-2'>
-                                {about}
-                            </p>
-                        </div>
-                    </div>
-                    {/* second row */}
-                    <div className='flex flex-col sm:grid sm:gap-x-5 sm:grid-flow-col'>
-                        <div className={`mt-5 border border-transparent rounded-2xl px-5 py-3 ${backgroudTextColor}`}>
-                            <p className='text-sm font-press-start'>Abilities</p>
-                            <div className='mt-3 px-3 space-y-2 font-quicksand font-semibold text-sm'>
-                                {
-                                    abilities.map((ability) => (
-                                        <li>{ability.ability}</li>
-                                    ))
-                                }
-                            </div>
-                        </div> 
-                        <div className={`mt-5 border border-transparent rounded-2xl px-5 py-3 ${backgroudTextColor}`}>
-                            <p className='text-sm font-press-start'>Base Stats</p>
-                            <div className='grid grid-cols-3 mt-2'>
-                                <div>
-                                    <p className='font-medium text-red'>ATTACK</p>
-                                    <p className='font-quicksand font-semibold text-sm text-gray-700'>{baseStats.attack}</p>
-                                </div>
-                                <div>
-                                    <p className='font-medium text-red'>DEFENSE</p>
-                                    <p className='font-quicksand font-semibold text-sm text-gray-700'>{baseStats.defense}</p>
-                                </div>
-                                <div>
-                                    <p className='font-medium text-red'>HP</p>
-                                    <p className='font-quicksand font-semibold text-sm text-gray-700'>{baseStats.hp}</p>
-                                </div>
-                                <div>
-                                    <p className='font-medium text-red'>SPECIAL-ATTACK</p>
-                                    <p className='font-quicksand font-semibold text-sm text-gray-700'>{baseStats.specialAttack}</p>
-                                </div>
-                                <div>
-                                    <p className='font-medium text-red'>SPECIAL-DEFENSE</p>
-                                    <p className='font-quicksand font-semibold text-sm text-gray-700'>{baseStats.specialDefense}</p>
-                                </div>
-                                <div>
-                                    <p className='font-medium text-red'>SPEED</p>
-                                    <p className='font-quicksand font-semibold text-sm text-gray-700'>{baseStats.speed}</p>
-                                </div>
-                            </div>
-                        </div>
-                    </div>
-                    {/* third row */}
-                    <div className={`mt-5 border border-transparent rounded-2xl px-5 py-3 ${backgroudTextColor}`}>
-                        <p className='text-sm font-press-start'>Evolution</p>
-                        <div className='mt-4 inline-flex'>
-                            {
-                                evolution.map((pokemon, index) => (
-                                    index === 0 ?
-                                    <div className='flex flex-col'>
-                                        <a href={`${pokemon.href}`} className={`${circleColor} inline-flex items-center p-4 border border-transparent rounded-full shadow-lg`}>
-                                            <img 
-                                                src={pokemon.src}
-                                                alt={pokemon.alt}
-                                                className='h-20 w-20'
-                                            />
-                                        </a>
-                                        <div className='text-center mt-2'>
-                                            <p className='text-xl font-vt323 pl-3'>{pokemon.name}</p>
-                                        </div>
-                                    </div> :
-                                    <>
-                                        <div className='mx-3 my-11'>
-                                            <ArrowNarrowRightIcon className='h-5 w-5' />
-                                        </div>
-                                        <div className='flex flex-col'>
-                                            <a href={pokemon.href} className={`${circleColor} inline-flex items-center p-4 border border-transparent rounded-full shadow-lg`}>
-                                                <img 
-                                                    src={pokemon.src} 
-                                                    alt={pokemon.alt}
-                                                    className='h-20 w-20'
+                            <div className='grid grid-flow-row auto-rows-max'>
+                                {/* first row */}
+                                <div className='grid grid-cols-1 xl:grid-cols-2'>
+                                    <div className='inline-flex items-center'>
+                                        <img src={`${pokemonImg.src}`} alt={`${pokemonImg.alt}`} className='h-36 w-36 rounded-2xl mx-4' />
+                                        <div>
+                                            <p className='text-2xl font-normal font-quicksand'>{id}</p>
+                                            <p className='text-lg font-press-start pt-2'>{name}</p>
+                                            <div className='space-x-1 flex flex-wrap items-center mt-5'>
+                                                <BasicBadge 
+                                                    text={`${tag.text}`} 
+                                                    textStyle='text-xl leading-8 font-medium text-white font-vt323' 
+                                                    backgroundColor={`${tag.backgroundColor}`}
                                                 />
-                                            </a>
-                                            <div className='text-center mt-2'>
-                                                <p className='text-xl font-vt323 pl-3'>{pokemon.name}</p>
+                                                {
+                                                    types.map((type, index) => (
+                                                        <CircularBadge 
+                                                            key={index}
+                                                            src={`${type.src}`} 
+                                                            backgroundColor={`${type.backgroundColor}`}
+                                                            tooltip={`${type.tooltip}`}
+                                                        />
+                                                    ))
+                                                }
                                             </div>
-                                        </div> 
-                                    </>    
-                                ))
-                            }
+                                        </div>
+                                    </div>
+                                    <div className='border-l-2 border-gray-600 flex flex-wrap content-center pr-20 pl-10 mt-4 xl:ml-8'>
+                                        <p className='text-topic'>About</p>
+                                        <p className='text-detail pt-2 text-gray-700'>
+                                            {about}
+                                        </p>
+                                    </div>
+                                </div>
+                                {/* second row */}
+                                <div className='flex flex-col sm:grid sm:gap-x-5 sm:grid-flow-col'>
+                                    <div className={`mt-5 border border-transparent rounded-2xl px-5 py-3 ${backgroudTextColor}`}>
+                                        <p className='text-topic'>Abilities</p>
+                                        <div className='mt-3 px-3 space-y-2 ftext-detail'>
+                                            {
+                                                abilities.map((ability, index) => (
+                                                    <li key={index}>{ability.ability}</li>
+                                                ))
+                                            }
+                                        </div>
+                                    </div> 
+                                    <div className={`mt-5 border border-transparent rounded-2xl px-5 py-3 ${backgroudTextColor}`}>
+                                        <p className='text-topic'>Base Stats</p>
+                                        <div className='grid grid-cols-3 mt-2'>
+                                            <div>
+                                                <p className='base-stats-title'>ATTACK</p>
+                                                <p className='base-stats-value'>{baseStats.attack}</p>
+                                            </div>
+                                            <div>
+                                                <p className='base-stats-title'>DEFENSE</p>
+                                                <p className='base-stats-value'>{baseStats.defense}</p>
+                                            </div>
+                                            <div>
+                                                <p className='base-stats-title'>HP</p>
+                                                <p className='base-stats-value'>{baseStats.hp}</p>
+                                            </div>
+                                            <div>
+                                                <p className='base-stats-title'>SPECIAL-ATTACK</p>
+                                                <p className='base-stats-value'>{baseStats.specialAttack}</p>
+                                            </div>
+                                            <div>
+                                                <p className='base-stats-title'>SPECIAL-DEFENSE</p>
+                                                <p className='base-stats-value'>{baseStats.specialDefense}</p>
+                                            </div>
+                                            <div>
+                                                <p className='base-stats-title'>SPEED</p>
+                                                <p className='base-stats-value'>{baseStats.speed}</p>
+                                            </div>
+                                        </div>
+                                    </div>
+                                </div>
+                                {/* third row */}
+                                <div className={`mt-5 border border-transparent rounded-2xl px-5 py-3 ${backgroudTextColor}`}>
+                                    <p className='text-topic'>Evolution</p>
+                                    <div className='mt-4 inline-flex'>
+                                        {
+                                            evolution.map((pokemon, index) => (
+                                                <>
+                                                    { index !== 0 &&
+                                                        <div className='mx-3 my-11'>
+                                                            <ArrowNarrowRightIcon className='h-5 w-5' />
+                                                        </div>
+                                                    }
+                                                    <div key={index} className='flex flex-col'>
+                                                        <a 
+                                                            href={`${pokemon.href}`} 
+                                                            className={`${circleColor} inline-flex items-center p-4 border border-transparent rounded-full shadow-lg transition duration-500 ease-in-out transform hover:scale-110`}
+                                                        >
+                                                            <img src={pokemon.src} alt={pokemon.alt} className='h-20 w-20' />
+                                                        </a>
+                                                        <div className='text-center mt-2'>
+                                                            <p className='text-xl font-vt323 pl-3'>{pokemon.name}</p>
+                                                        </div>
+                                                    </div> 
+                                                </>    
+                                            ))
+                                        }
+                                    </div>
+                                </div>
+                            </div>
                         </div>
-                    </div>
+                    </Transition.Child>
                 </div>
-            </div>
-        </div>
+            </Dialog>
+        </Transition.Root>   
     )
 }
 
