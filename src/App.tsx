c20d9ba8e00125f28e88607f183afdc1029880d4
pluem <tw_pluem@hotmail.co.th>
2021-07-22 10:23:12 +0700

    [add] homepage

--- src/App.tsx
+++ src/App.tsx
@@ -5,20 +5,23 @@ import Register from "./screens/register"
 import Login from "./screens/login"
 import { BrowserRouter, Route, Switch } from "react-router-dom"
 import { NavLink } from "react-router-dom"
+import Homepage from "./screens/home";
 
 function App() {
   return (
     <BrowserRouter>
     <div className="App">
           <Switch>
-            {/* <Route exact path={["/", "/home"]} component={TestComponent} /> */}
-            <Route exact path="/" component={Register} />
-            {/* <Route exact path='/' component={Login} /> */}
+            <Route exact path="/" component={Homepage} />
+            <Route path="/register" component={Register} />
+            <Route path="/login" component={Login} />
+            <Route path="/test-component" component={TestComponent}/>
           </Switch>
-      {/* <header className="App-header">
-        <NavLink className="p-2 m-3 text-white bg-blue-dark rounded-md hover:bg-blue-dark" exact to='/'> Test Component </NavLink>
+      <header className="App-header">
+        <NavLink className="p-2 m-3 text-white bg-blue-dark rounded-md hover:bg-blue-dark" to='/test-component'> Test Component </NavLink>
         <NavLink className="p-2 text-white bg-blue-dark rounded-md hover:bg-blue-dark" to='/register'> Test Register </NavLink>
-      </header> */}
+        <NavLink className="p-2 text-white bg-blue-dark rounded-md hover:bg-blue-dark" to='/login'> Test Login </NavLink>
+      </header>
     </div>
     </BrowserRouter>
   );
