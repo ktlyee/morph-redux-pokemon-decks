81f6ea16a59fbcbe99091060f4988e3659e2286c
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-08-02 10:30:43 +0700

    [delete]: unused file

--- src/app/store.ts
+++ src/app/store.ts
@@ -1,14 +1,9 @@
 import { configureStore } from '@reduxjs/toolkit'
 import authReducer from '../reducers/auth'
-import { pokemonApiSlice } from '../reducers/pokemon'
 
 export const store = configureStore({
   reducer: {
       auth: authReducer,
-      [pokemonApiSlice.reducerPath]: pokemonApiSlice.reducer
-  },
-  middleware: (getDefaultMiddleware) => {
-    return getDefaultMiddleware().concat(pokemonApiSlice.middleware)
   }
 })
 
