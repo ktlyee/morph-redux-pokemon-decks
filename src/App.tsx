a3b8933842c662c6e1f29ab1df9ca31f47721af9
Krititya Onbun-ua <krittiya.on@mail.kmutt.ac.th>
2021-08-02 10:13:03 +0700

    [move]: BrowserRouter to main.tsx

--- src/App.tsx
+++ src/App.tsx
@@ -2,20 +2,18 @@ import React from "react";
 import "./App.css";
 import Register from "./screens/register"
 import Login from "./screens/login"
-import { BrowserRouter, Route, Switch } from "react-router-dom"
+import { Route, Switch } from "react-router-dom"
 import Homepage from "./screens/home";
 
 function App() {
   return (
-    <BrowserRouter>
-      <div className="App">
-            <Switch>
-              <Route exact path="/home" component={Homepage} />
-              <Route exact path="/register" component={Register} />
-              <Route exact path="/" component={Login} />
-            </Switch>
-      </div>
-    </BrowserRouter>
+    <div className="App">
+      <Switch>
+        <Route exact path="/home" component={Homepage} />
+        <Route exact path="/register" component={Register} />
+        <Route exact path="/" component={Login} />
+      </Switch>
+    </div>
   )
 }
 
--- src/main.tsx
+++ src/main.tsx
@@ -2,13 +2,16 @@ import React from 'react'
 import ReactDOM from 'react-dom'
 import { Provider } from 'react-redux'
 import { store } from './app/store'
+import { BrowserRouter } from 'react-router-dom'
 import './index.css'
 import App from './App'
 
 ReactDOM.render(
   <React.StrictMode>
     <Provider store={store}>
-      <App />
+      <BrowserRouter>
+        <App /> 
+      </BrowserRouter>
     </Provider>
   </React.StrictMode>,
   document.getElementById('root')
